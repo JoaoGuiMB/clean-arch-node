1e8b06e18b9d150980f184cde53476280c70ab54
JoaoGuiMB <juaogui159@gmail.com>
2024-03-07 20:43:53 -0300

    feat: validate order fields

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -11,6 +11,20 @@ export default class Order {
     this._customerId = customerId;
     this._items = items;
     this._total = this.total();
+    this.validate();
+  }
+
+  validate(): boolean {
+    if (!this._id) {
+      throw new Error("Id is required");
+    }
+    if (!this._customerId) {
+      throw new Error("CustomerId is required");
+    }
+    if (this._items.length === 0) {
+      throw new Error("Item quantity must be greater than 0");
+    }
+    return true;
   }
 
   total() {
