fbacb48a6409dbde0c147166859d6617953a3735
JoaoGuiMB <juaogui159@gmail.com>
2024-04-28 19:48:34 -0300

    test: change create product integration test

--- src/usecase/product/create/create.product.integration.spec.ts
+++ src/usecase/product/create/create.product.integration.spec.ts
@@ -2,6 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
 import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
 import ProductFactory from "../../../domain/product/factory/product.factory";
+import CreateProductUseCase from "./create.product.usecase";
 
 describe("Test find customer use case", () => {
   let sequelize: Sequelize;
@@ -24,17 +25,30 @@ describe("Test find customer use case", () => {
 
   it("should create a product", async () => {
     const productRepository = new ProductRepository();
-    const product = ProductFactory.create("a", "Product 1", 100);
-    await productRepository.create(product);
+    const useCase = new CreateProductUseCase(productRepository);
+    const input = {
+      type: "a",
+      name: "Product 1",
+      price: 100,
+    };
 
-    const productModel = await ProductModel.findOne({
-      where: { id: product.id },
+    const result = await useCase.execute(input);
+
+    expect(result).toStrictEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+      type: input.type,
     });
 
-    expect(productModel?.toJSON()).toStrictEqual({
-      id: product.id,
-      name: product.name,
-      price: product.price,
+    const productFound = await ProductModel.findOne({
+      where: { id: result.id },
+    });
+
+    expect(productFound?.toJSON()).toStrictEqual({
+      id: result.id,
+      name: input.name,
+      price: input.price,
     });
   });
 });
