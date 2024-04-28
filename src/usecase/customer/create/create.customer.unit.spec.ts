161871b3647b4d7afb5c18fea1739a01253b2eac
JoaoGuiMB <juaogui159@gmail.com>
2024-04-28 10:28:45 -0300

    test: create customer use case

---
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,52 @@
+import CustomerCreateUseCase from "./create.customer.usecase";
+
+const input = {
+  name: "John Doe",
+  address: {
+    street: "Main Street",
+    city: "New York",
+    number: 123,
+    zip: "12345-678",
+  },
+};
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    update: jest.fn(),
+    create: jest.fn(),
+  };
+};
+
+describe("Unit test create customer", () => {
+  it("should create a new customer", async () => {
+    const repository = MockRepository();
+    const customerCreateUseCase = new CustomerCreateUseCase(repository);
+    const result = await customerCreateUseCase.execute(input);
+
+    const output = {
+      id: expect.any(String),
+      name: input.name,
+      address: {
+        street: input.address.street,
+        city: input.address.city,
+        number: input.address.number,
+        zip: input.address.zip,
+      },
+    };
+
+    expect(result).toEqual(output);
+  });
+
+  it("should throw an error when name is missing", async () => {
+    const repository = MockRepository();
+    const customerCreateUseCase = new CustomerCreateUseCase(repository);
+
+    input.name = "";
+
+    await expect(customerCreateUseCase.execute(input)).rejects.toThrow(
+      "Name is required"
+    );
+  });
+});
