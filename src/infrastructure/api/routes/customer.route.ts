906040ebaff7ed9a88467df7d6dd5d9fb9b66a0b
JoaoGuiMB <juaogui159@gmail.com>
2024-04-30 19:36:48 -0300

    feat: list customers route

--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
+import ListCustomerUsecase from "../../../usecase/customer/list/list.customer.usecase";
 
 export const customerRoute = express.Router();
 
@@ -13,6 +14,16 @@ customerRoute.post("/", async (req: Request, res: Response) => {
     };
     const customer = await useCase.execute(customerDto);
     res.status(200).json(customer);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+customerRoute.get("/", async (req: Request, res: Response) => {
+  const useCase = new ListCustomerUsecase(new CustomerRepository());
+  try {
+    const customers = await useCase.execute({});
+    res.status(200).json(customers);
   } catch (error) {
     res.status(500).send(error);
   }
