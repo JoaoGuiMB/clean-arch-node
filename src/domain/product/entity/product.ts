c99d1549d7d3c2c95654cb7f5a125add3b608833
JoaoGuiMB <juaogui159@gmail.com>
2024-05-01 18:56:51 -0300

    feat: implementing product validations with yup

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
 export default class Product extends Entity implements ProductInterface {
@@ -36,25 +37,7 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate(): boolean {
-    if (!this.id) {
-      this.notification.addError({
-        message: "Id is required",
-        context: "product",
-      });
-    }
-    if (!this._name) {
-      this.notification.addError({
-        message: "Name is required",
-        context: "product",
-      });
-    }
-
-    if (this._price <= 0) {
-      this.notification.addError({
-        message: "Price must be greater than 0",
-        context: "product",
-      });
-    }
+    ProductValidatorFactory.create().validate(this);
     return true;
   }
 }
