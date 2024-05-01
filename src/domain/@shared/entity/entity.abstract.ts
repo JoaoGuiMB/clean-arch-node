c8dd371656341b715a1d7ded61ddddb43f4127c9
JoaoGuiMB <juaogui159@gmail.com>
2024-05-01 18:50:54 -0300

    feat: creating validator

--- src/domain/@shared/entity/entity.abstract.ts
+++ src/domain/@shared/entity/entity.abstract.ts
@@ -2,7 +2,7 @@ import Notification from "../notification/notification";
 
 export default abstract class Entity {
   public id: string;
-  protected notification: Notification;
+  public notification: Notification;
 
   constructor(id: string) {
     this.id = id;
